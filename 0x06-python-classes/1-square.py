71ea43b5dd8b57048975b243771c7f65bbc966d4
root <sanya4reel@gmail.com>
2022-07-20 05:04:35 -0700

    Class with instant icorrevcto

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,18 @@
 
 
 class Square:
+    """ A square class
+
+    Attributes:
+        __size (int): size of the side of the square
+    """
+
     def __init__(self, size):
+        """ Initializes a squarre
+
+         agrs:
+            size (int): size of sqaure side
+
+        Returns: none
+        """
         self.__size = size
