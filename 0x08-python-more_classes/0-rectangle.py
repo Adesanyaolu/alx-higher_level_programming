85cde414c7978896b9df1010f022a4198fcdf01b
root <sanya4reel@gmail.com>
2022-07-25 16:27:00 -0700

    Task 0 and 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This defines a rectangle class.
+"""
+
+
+class Rectangle:
+    """A Rectangle class."""
+    def __init__(self):
+        """Initializes class."""
+        pass
