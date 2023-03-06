aafeb738d147b14b6986e46944168bd171ad9bb3
adesanyaolu <sanya4reel@gmail.com>
2023-03-06 12:21:49 +0100

    Tasks 8 and 9

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str.split(", ")[2].split()[0] + " " + str.split(", ")[3]
+
+print(str.index)
